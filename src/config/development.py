e03d73a64455e52eddd9822ce10e50d3c3993ffa
Mamadou <mamadou.web.dev@gmail.com>
2026-08-24 20:21:54 +0200

    fix(config): use memory storage for rate limiter in development

--- src/config/development.py
+++ src/config/development.py
@@ -9,3 +9,4 @@ class DevelopmentConfig(BaseConfig):
     DEBUG: bool = True
     DATABASE_URL: str = "sqlite:///./store_up_dev.db"
     RATE_LIMIT: str = "1000 per hour"  # relaxed for dev
+    RATE_LIMIT_STORAGE_URL: str = "memory://"
