e7dd755b7656c68ee1682c5d1a48695f5c6ecb9e
Mamadou <mamadou.web.dev@gmail.com>
2026-08-31 14:12:13 +0200

    Update role.py

--- src/domains/rbac/services/role.py
+++ src/domains/rbac/services/role.py
@@ -49,5 +49,6 @@ class RoleService(BaseService):
             role = uow.roles.get(role_id)
             if not role:
                 raise RoleNotFound(f"Role '{role_id}' not found.")
+            uow.roles.delete(role)
             uow.track(role)
             return ServiceResult(data=None)
